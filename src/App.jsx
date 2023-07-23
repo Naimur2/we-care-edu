cbb29bf0c24ca681fd9b882ace43b932c94401f7
naimur2 <naimurrahaman248@gmail.com>
2023-07-23 12:37:03 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,87 @@
-import React from "react";
-// import { Counter } from './features/counter/Counter';
-import { Stack } from "@mui/material";
-import { useSelector } from "react-redux";
-import "./App.css";
-import "./font.css";
-import HomePage from "./page/HomePage";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { MessengerChat } from "react-messenger-chat-plugin";
+// import React from "react";
+// // import { Counter } from './features/counter/Counter';
+// import { Stack } from "@mui/material";
+// import { useSelector } from "react-redux";
+// import "./App.css";
+// import "./font.css";
+// import HomePage from "./page/HomePage";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+// import { MessengerChat } from "react-messenger-chat-plugin";
 
-function App() {
-    const { isDarkMode } = useSelector((state) => state.darkMode);
+// function App() {
+//     const { isDarkMode } = useSelector((state) => state.darkMode);
 
-    const [currentWindowWidth, setCurrentWindowWidth] = React.useState(
-        window.innerWidth
-    );
+//     const [currentWindowWidth, setCurrentWindowWidth] = React.useState(
+//         window.innerWidth
+//     );
 
-    React.useEffect(() => {
-        AOS.init({
-            duration: 1000,
-            disable: () => window.innerWidth < 1024,
-        });
-    }, [currentWindowWidth]);
+//     React.useEffect(() => {
+//         AOS.init({
+//             duration: 1000,
+//             disable: () => window.innerWidth < 1024,
+//         });
+//     }, [currentWindowWidth]);
 
-    React.useEffect(() => {
-        const handleResize = () => {
-            setCurrentWindowWidth(window.innerWidth);
-        };
+//     React.useEffect(() => {
+//         const handleResize = () => {
+//             setCurrentWindowWidth(window.innerWidth);
+//         };
 
-        window.addEventListener("resize", handleResize);
+//         window.addEventListener("resize", handleResize);
 
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+//         return () => {
+//             window.removeEventListener("resize", handleResize);
+//         };
+//     }, []);
 
-    return (
-        <Stack component={"main"} bgcolor={isDarkMode ? "#0D0D0D" : "#fff"}>
-            <HomePage />
-            <MessengerChat
-                pageId="109268111769502"
-                appId="109268111769502"
-                language="sv_SE"
-                themeColor={"#000000"}
-                bottomSpacing={300}
-                loggedInGreeting="loggedInGreeting"
-                loggedOutGreeting="loggedOutGreeting"
-                greetingDialogDisplay={"show"}
-                debugMode={true}
-                onMessengerShow={() => {
-                    console.log("onMessengerShow");
-                }}
-                onMessengerHide={() => {
-                    console.log("onMessengerHide");
-                }}
-                onMessengerDialogShow={() => {
-                    console.log("onMessengerDialogShow");
-                }}
-                onMessengerDialogHide={() => {
-                    console.log("onMessengerDialogHide");
-                }}
-                onMessengerMounted={() => {
-                    console.log("onMessengerMounted");
-                }}
-                onMessengerLoad={() => {
-                    console.log("onMessengerLoad");
-                }}
-            />
-        </Stack>
-    );
+//     return (
+//         <Stack component={"main"} bgcolor={isDarkMode ? "#0D0D0D" : "#fff"}>
+//             <HomePage />
+//             <MessengerChat
+//                 pageId="109268111769502"
+//                 appId="109268111769502"
+//                 language="sv_SE"
+//                 themeColor={"#000000"}
+//                 bottomSpacing={300}
+//                 loggedInGreeting="loggedInGreeting"
+//                 loggedOutGreeting="loggedOutGreeting"
+//                 greetingDialogDisplay={"show"}
+//                 debugMode={true}
+//                 onMessengerShow={() => {
+//                     console.log("onMessengerShow");
+//                 }}
+//                 onMessengerHide={() => {
+//                     console.log("onMessengerHide");
+//                 }}
+//                 onMessengerDialogShow={() => {
+//                     console.log("onMessengerDialogShow");
+//                 }}
+//                 onMessengerDialogHide={() => {
+//                     console.log("onMessengerDialogHide");
+//                 }}
+//                 onMessengerMounted={() => {
+//                     console.log("onMessengerMounted");
+//                 }}
+//                 onMessengerLoad={() => {
+//                     console.log("onMessengerLoad");
+//                 }}
+//             />
+//         </Stack>
+//     );
+// }
+
+// export default App;
+
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+        <h1>
+            Welcome to Our Website
+        </h1>
+    </div>
+  )
 }
-
-export default App;
